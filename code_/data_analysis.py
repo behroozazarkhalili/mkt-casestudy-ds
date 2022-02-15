2d2671f3bbd5992ddec4124961af7427b7b6b27d
ermia <ermiaaazarkhalili@gmail.com>
2022-02-15 20:36:31 +0330

    Write a method  in the class EDA to remove the columns enjoying more than a specific threshold of missing values.

--- code_/data_analysis.py
+++ code_/data_analysis.py
@@ -35,4 +35,34 @@ class EDA(object):
         self.numerical_cols = [col for col in self.df.columns if col not in self.categorical_cols]
         return
 
+    def remove_missing_value(self, is_drop=True) -> pd.DataFrame:
+        """
+        get information regarding missing values of each column of a dataframe.
+        the method remove columns enjoying more than missing_value_pct_threshold missing values.
+        :param: is_drop: if True, the method will drop the columns enjoying more than missing_value_pct_threshold missing values.
+        :return: dataframe with missing values information.
+        """
+        self.get_cat_num_features()
+
+        # Create dataframe with missing values information.
+        missing_value_df = pd.DataFrame(self.df.isnull().sum()).reset_index()
+        missing_value_df.columns = ['column_name', 'missing_count']
+        missing_value_df['missing_pct'] = (missing_value_df['missing_count'] / len(self.df)) * 100
+
+        # Sort the dataframe by missing_pct in descending order.
+        missing_value_df = missing_value_df.sort_values(by='missing_pct', ascending=False)
+
+        # Write the missing value information to a csv file.
+        missing_value_df.to_csv(self.config.get("missing_value_df_path"), index=False)
+
+        # Get the columns including more than missing_value_pct_threshold missing values.
+        to_drop = missing_value_df[missing_value_df['missing_pct'] >= self.missing_value_pct_threshold]['column_name'].tolist()
+
+        # Drop the columns with more than missing_value_pct_threshold missing values if is_drop=True.
+        if is_drop and len(to_drop) > 0:
+            self.df.drop(to_drop, axis=1, inplace=True)
+            self.get_cat_num_features()
+
+        return missing_value_df
+
 
